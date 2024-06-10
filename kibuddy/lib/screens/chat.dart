db8e56f62881202abd9399c105696723ee091fe5
Marc <inf3462@hs-worms.de>
2024-06-10 17:25:17 +0200

    added chat header

--- kibuddy/lib/screens/chat.dart
+++ kibuddy/lib/screens/chat.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -33,6 +34,40 @@ class ChatPage extends StatelessWidget {
           ],
         ),
         titleSpacing: -5.0,
+        actions: [
+          IconButton(
+            icon: Icon(CupertinoIcons.videocam),
+            color: Color(0xFF3297B7), // Set color for camera icon
+            iconSize: 36, // Adjust the size of the icon
+            onPressed: () {
+              // Add functionality for camera icon
+            },
+          ),
+          IconButton(
+            icon: Icon(CupertinoIcons.phone),
+            color: Color(0xFF3297B7), // Set color for telephone icon
+            iconSize: 24, // Adjust the size of the icon
+            onPressed: () {
+              // Add functionality for telephone icon
+            },
+          ),
+          IconButton(
+  icon: Padding(
+    padding: EdgeInsets.only(right: 10.0), // Add 20 pixels of padding on the right side
+    child: Transform.translate(
+      offset: Offset(0.0, -1.0), // Adjust the offset to move the icon higher
+      child: Image.asset(
+        'assets/images/robot.png', // Path to your PNG icon
+        width: 28, // Adjust width as needed
+        height: 28, // Adjust height as needed
+      ),
+    ),
+  ),
+  onPressed: () {
+    // Add functionality for the robot icon
+  },
+),
+        ],
       ),
       body: const Center(
         child: Text('Welcome to the chat page!'),
