9adf6ceb5d525a00f8c9be2f284a640a26557ee0
Marc <inf3462@hs-worms.de>
2024-06-10 16:29:49 +0200

    add avatar and megan

--- kibuddy/lib/screens/chat.dart
+++ kibuddy/lib/screens/chat.dart
@@ -7,23 +7,32 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new),
+          color: Color(0xFF3297B7), // Custom color for the arrow icon
+          onPressed: () {
+            Navigator.of(context).pop(); // This will close the current screen
+          },
+        ),
         title: Row(
           children: [
-            IconButton(
-              icon: const Icon(Icons.arrow_back_ios_new),
-              color: Color(0xFF3297B7), // Custom color for the arrow icon
-              onPressed: () {
-                Navigator.of(context).pop(); // This will close the current screen
-              },
-            ),
-            const SizedBox(width: 0), // Add space between icons
             CircleAvatar(
               radius: 20, // Adjust size as needed
               backgroundColor: Colors.grey[300], // Customize background color
               child: Icon(Icons.person), // Optionally, you can add an icon or image
             ),
+            const SizedBox(width: 10), // Add some space between avatar and text
+            Text(
+  'Megan',
+  style: TextStyle(
+    fontSize: 20,
+    color: const Color.fromARGB(255, 0, 0, 0),
+    fontWeight: FontWeight.w500, // Lighter bold weight
+  ),
+),
           ],
         ),
+        titleSpacing: -5.0,
       ),
       body: const Center(
         child: Text('Welcome to the chat page!'),
