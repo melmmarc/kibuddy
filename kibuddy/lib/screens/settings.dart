2da5ac3e118e0849fc85bca5727445e9b723c22a
Marc <inf3462@hs-worms.de>
2024-06-11 16:20:42 +0200

    enabled toggle button functionality

--- kibuddy/lib/screens/settings.dart
+++ kibuddy/lib/screens/settings.dart
@@ -1,10 +1,17 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
-// ignore: unused_import
-import 'kibuddy.dart';
 import 'package:flutter/cupertino.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool isSwitched = true; // State variable to track the switch state
 
   @override
   Widget build(BuildContext context) {
@@ -40,150 +47,139 @@ class SettingsPage extends StatelessWidget {
             ),
             Expanded(
               child: ListView.builder(
-                itemCount: 6, // Number of white boxes
-
-
-itemBuilder: (context, index) {
-  return index == 0
-      ? Transform.translate(
-          offset: const Offset(0, -20),
-          child: Container(
-            height: 85, // Adjusted height to 85px
-            color: Colors.white,
-            margin: const EdgeInsets.only(bottom: 5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 25, top: 10), // Add 25px margin to the left and 10px margin to the top
-                      child: Image.asset(
-                        'assets/images/kibuddy-circled.png', // Your image path
-                        width: 50, // Adjust width as needed
-                        height: 50, // Adjust height as needed
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 20, top: 20), // Add 20px margin to the left and 20px margin to the top
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'KI-Buddy',
-                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500,),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                Transform.translate(
-                  offset: const Offset(0, -5), // Move the text 5px higher
-                  child: const Padding(
-                    padding: EdgeInsets.only(left: 22, top: 5), // Add 22px margin to the left and 5px margin to the top
-                    child: Text(
-                      'Bearbeiten',
-                      style: TextStyle(fontSize: 10, color: Color(0xFF3297B7),fontWeight: FontWeight.w500,),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        )
-      : Container(
-          height: 55, // Adjust height as needed
-          color: Colors.white,
-          margin: index == 1
-              ? const EdgeInsets.fromLTRB(0, 0, 0, 20) // Add 20px more margin at the bottom for Box 2
-              : const EdgeInsets.symmetric(vertical: 2), // Standard margin
-          child: index == 1
-              ? const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 25), // Add 25px margin to the left
-                      child: Icon(
-                        Icons.language, // iOS language/world icon
-                        size: 28,
-                        color: Color(0xFF3297B7), // Adjust icon color as needed
-                      ),
-                    ),
-                    SizedBox(width: 15), // Add spacing between the icon and the text
-                    Text(
-                      'Sprache', // Change from "Sprache" to "Positiv"
-                      style: TextStyle(fontSize: 16, color: Color(0xFF232323), fontWeight: FontWeight.w500),
-                    ),
-                    Spacer(),
-                    Text(
-                      'Deutsch', // Text next to the arrow pointing right
-                      style: TextStyle(fontSize: 16, color: Color(0xFFC8C7CC), fontWeight: FontWeight.w500),
-                    ),
-                    SizedBox(width: 10), // Add spacing between the text and the arrow
-                    Padding(
-                      padding: EdgeInsets.only(right: 10), // Add 10px margin to the right
-                      child: Icon(
-                        Icons.arrow_forward_ios, // iOS arrow pointing to the right
-                        size: 20,
-                        color: Color(0xFFC8C7CC), // Adjust icon color as needed
-                      ),
-                    ),
-                  ],
-                )
-              : index == 2
-                  ? Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 30), // Add 25px margin to the left
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  return index == 0
+                      ? Transform.translate(
+                          offset: const Offset(0, -20),
                           child: Container(
-                            width: 16, // Adjust the width of the green circle
-                            height: 16, // Adjust the height of the green circle
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Color(0xFF00E809)
+                            height: 85,
+                            color: Colors.white,
+                            margin: const EdgeInsets.only(bottom: 5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 25, top: 10),
+                                      child: Image.asset(
+                                        'assets/images/kibuddy-circled.png',
+                                        width: 50,
+                                        height: 50,
+                                      ),
+                                    ),
+                                    const Padding(
+                                      padding: EdgeInsets.only(left: 20, top: 20),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            'KI-Buddy',
+                                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Transform.translate(
+                                  offset: const Offset(0, -5),
+                                  child: const Padding(
+                                    padding: EdgeInsets.only(left: 22, top: 5),
+                                    child: Text(
+                                      'Bearbeiten',
+                                      style: TextStyle(fontSize: 10, color: Color(0xFF3297B7), fontWeight: FontWeight.w500),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ),
-                        const SizedBox(width: 22), // Add spacing between the green circle and the text
-                        const Text(
-                          'Positiv', // Change from "Sprache" to "Positiv"
-                          style: TextStyle(fontSize: 16, color: Color(0xFF232323), fontWeight: FontWeight.w500),
-                        ),
-                        const Spacer(),
-                        Padding(
-                          padding: const EdgeInsets.only(right: 10), // Add 10px margin to the right
-                          child: Transform.scale(
-                            scale: 0.8, // Adjust the scale to make the switch smaller
-                            child: CupertinoSwitch(
-                              value: true, // Toggle button turned on
-                              onChanged: (value) {}, // Function to handle toggle changes
-                              activeColor: const Color(0xFF6edc5f), // Color of the toggle when turned on
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
-                  : Center(
-                      child: Text(
-                        'Box ${index + 1}',
-                        style: const TextStyle(fontSize: 18),
-                      ),
-                    ),
-        );
-},
-
-
-
-
-
-
-
-
-
-
-
-
-
+                        )
+                      : Container(
+                          height: 55,
+                          color: Colors.white,
+                          margin: index == 1
+                              ? const EdgeInsets.fromLTRB(0, 0, 0, 20)
+                              : const EdgeInsets.symmetric(vertical: 2),
+                          child: index == 1
+                              ? const Row(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(left: 25),
+                                      child: Icon(
+                                        Icons.language,
+                                        size: 28,
+                                        color: Color(0xFF3297B7),
+                                      ),
+                                    ),
+                                    SizedBox(width: 15),
+                                    Text(
+                                      'Sprache',
+                                      style: TextStyle(fontSize: 16, color: Color(0xFF232323), fontWeight: FontWeight.w500),
+                                    ),
+                                    Spacer(),
+                                    Text(
+                                      'Deutsch',
+                                      style: TextStyle(fontSize: 16, color: Color(0xFFC8C7CC), fontWeight: FontWeight.w500),
+                                    ),
+                                    SizedBox(width: 10),
+                                    Padding(
+                                      padding: EdgeInsets.only(right: 10),
+                                      child: Icon(
+                                        Icons.arrow_forward_ios,
+                                        size: 20,
+                                        color: Color(0xFFC8C7CC),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              : index == 2
+                                  ? Row(
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.only(left: 30),
+                                          child: Container(
+                                            width: 16,
+                                            height: 16,
+                                            decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              color: Color(0xFF00E809),
+                                            ),
+                                          ),
+                                        ),
+                                        const SizedBox(width: 22),
+                                        const Text(
+                                          'Positiv',
+                                          style: TextStyle(fontSize: 16, color: Color(0xFF232323), fontWeight: FontWeight.w500),
+                                        ),
+                                        const Spacer(),
+                                        Padding(
+                                          padding: const EdgeInsets.only(right: 10),
+                                          child: Transform.scale(
+                                            scale: 0.8,
+                                            child: CupertinoSwitch(
+                                              value: isSwitched,
+                                              onChanged: (value) {
+                                                setState(() {
+                                                  isSwitched = value; // Update the state variable
+                                                });
+                                              },
+                                              activeColor: const Color(0xFF6edc5f),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  : Center(
+                                      child: Text(
+                                        'Box ${index + 1}',
+                                        style: const TextStyle(fontSize: 18),
+                                      ),
+                                    ),
+                        );
+                },
               ),
             ),
           ],
